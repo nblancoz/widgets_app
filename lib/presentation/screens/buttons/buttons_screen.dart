18b3c67e8c83b8277f16f27debc073e08a565fb7
nblancoz <nblancozappala@gmail.com>
2024-09-03 23:39:27 +0200

    personalized button done

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -90,8 +90,31 @@ class _ButtonsView extends StatelessWidget {
                 iconColor: const WidgetStatePropertyAll(Colors.white),
               ),
             ),
-            // ToDo: Custom button
+            const CustomButton(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Hola Mundo', style: TextStyle(color: Colors.white)),
+          ),
         ),
       ),
     );
