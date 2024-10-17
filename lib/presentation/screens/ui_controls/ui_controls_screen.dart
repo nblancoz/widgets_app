9f66bd23e0ea04738d108ad3b2180a5bbc2a1ac4
nblancoz <nblancozappala@gmail.com>
2024-10-17 12:06:18 +0200

    screens created, exports added

---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UiControlsScreen extends StatelessWidget {
+  const UiControlsScreen({super.key});
+
+  static const name = 'ui_controls_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
